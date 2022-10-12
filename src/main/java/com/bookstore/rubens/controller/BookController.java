3f8410523be650defb5d64ae7dc45c30a4bf7e46
RubensFsousa <xansxxl@gmail.com>
2022-10-11 21:46:53 -0300

    fix: resolvin context path #master

--- src/main/java/com/bookstore/rubens/controller/BookController.java
+++ src/main/java/com/bookstore/rubens/controller/BookController.java
@@ -19,7 +19,7 @@ import javax.validation.Valid;
 @RequiredArgsConstructor
 @CrossOrigin(origins = "*")
 @Api(value = "API REST BOOKS")
-@RequestMapping("/BookStore")
+@RequestMapping("/bookstore")
 public class BookController {
 
     @Autowired
